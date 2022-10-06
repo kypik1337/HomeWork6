5d0f3be817958bffd3489e32c2ea4717a2e65e18
tema <kypidon71@gmail.com>
2022-10-06 22:04:41 +0300

    001

---
+++ zad1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Add numbers M");
+int[] ArrayNumbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+for (int i = 0; i < ArrayNumbers.Length; i++)
+{
+    if (ArrayNumbers[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($" Количество чисел > 0:= {count}");
